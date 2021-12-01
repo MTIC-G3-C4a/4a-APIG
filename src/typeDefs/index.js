3d95052e2b061d9446de6c8a14662f67a059ac5b
gramkamana <gramkamana108@gmail.com>
2021-12-01 14:57:35 -0500

    agregar typedefs doctor e index

---
+++ src/typeDefs/index.js
@@ -0,0 +1,8 @@
+
+const doctorTypeDefs = require('./doctor_type_defs');
+const enfermedadTypeDefs = require('./enfermedad_type_defs');
+const pacienteTypeDefs = require('./paciente_type_defs');
+
+const schemasArrays = [doctorTypeDefs, enfermedadTypeDefs, pacienteTypeDefs];
+
+module.exports = schemasArrays;
